473598ba816f95c5877942265cd0f683d865f24e
Richard Bishop <rick@richardbishop.me>
2017-04-16 12:11:43 +0100

    Add url handler for files

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,24 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/,
                 use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'sass-loader'],
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false
+                            }
+                        },
+                        'sass-loader'
+                    ],
                     fallback: 'style-loader'
                 })
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
             },
             {
                 test: /\.js$/,
